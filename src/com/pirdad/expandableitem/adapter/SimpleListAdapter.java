c4ebda879233ca0952cfeff4bda948333e108e54
Pirdad <pirdod@gmail.com>
2013-05-17 17:51:12 -0400

    Initial codes for setting and being able to drag the right hidden layout. Right now, animations don't work. ListView can't scroll up and down. And there's no way to tell the difference between up/down vs right/left movements.

--- src/com/pirdad/expandableitem/adapter/SimpleListAdapter.java
+++ src/com/pirdad/expandableitem/adapter/SimpleListAdapter.java
@@ -60,6 +60,7 @@ public class SimpleListAdapter extends BaseAdapter implements ExpandableItem.Ite
 
         View view_visible = tst_item.visible_view;
         View view_hidden = tst_item.hidden_view;
+        View view_right = tst_item.right_view;
 
         if (view_visible == null) {
             view_visible = inflater.inflate(R.layout.item_visible, null);
@@ -69,22 +70,27 @@ public class SimpleListAdapter extends BaseAdapter implements ExpandableItem.Ite
             view_hidden = inflater.inflate(R.layout.item_hidden, null);
             tst_item.hidden_view = view_hidden;
         }
+        if (view_right == null) {
+            view_right = inflater.inflate(R.layout.item_right, null);
+            tst_item.right_view = view_right;
+        }
 
         final ExpandableItem view = new ExpandableItem(context);
         view.setup.id = tst_item.id;
         view.setup.center_layout = view_visible;
         view.setup.bottom_layout = view_hidden;
+        view.setup.right_layout = view_right;
         view.setup.drag_gesture = ExpandableItem.DRAG_GESTURE.TWO_FINGER;
         view.setup.long_press_delay = 1000;
         view.setup.open_listener = this;
         view.setup.extra_params.put("ITEM_REF", tst_item);
-        view.setup.is_expanded = tst_item.is_open;
+        view.setup.is_expanded = tst_item.is_open; // this will reset view to either opened or closed after you scroll
 
-        TextView txt_visible = (TextView) view.setup.center_layout.findViewById(R.id.txt_visible);
-        TextView txt_hidden = (TextView) view.setup.bottom_layout.findViewById(R.id.txt_hidden);
+        //TextView txt_visible = (TextView) view.setup.center_layout.findViewById(R.id.txt_visible);
+        //TextView txt_hidden = (TextView) view.setup.bottom_layout.findViewById(R.id.txt_hidden);
 
-        txt_visible.setText(tst_item.title);
-        txt_hidden.setText(tst_item.detail);
+        //txt_visible.setText(tst_item.title);
+        //txt_hidden.setText(tst_item.detail);
 
         return view;
     }
@@ -94,7 +100,7 @@ public class SimpleListAdapter extends BaseAdapter implements ExpandableItem.Ite
 
         TestItem item = (TestItem) view.setup.extra_params.get("ITEM_REF");
         item.is_open = true;
-        Toast.makeText(context, "item: " + id + " is now open", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, "item: " + id + " is now open", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -102,7 +108,7 @@ public class SimpleListAdapter extends BaseAdapter implements ExpandableItem.Ite
 
         TestItem item = (TestItem) view.setup.extra_params.get("ITEM_REF");
         item.is_open = false;
-        Toast.makeText(context, "item: " + id + " is now closed", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, "item: " + id + " is now closed", Toast.LENGTH_SHORT).show();
     }
 
     public class TestItem {
@@ -114,6 +120,8 @@ public class SimpleListAdapter extends BaseAdapter implements ExpandableItem.Ite
         public boolean is_open = false;
         public View hidden_view;
         public View visible_view;
+        public View right_view;
+
         public TestItem(int id, String title, String detail) {
 
             this.id = id;
--- src/com/pirdad/expandableitem/view/ExpandableItem.java
+++ src/com/pirdad/expandableitem/view/ExpandableItem.java
@@ -7,8 +7,8 @@ import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,14 +27,17 @@ public class ExpandableItem extends View {
 
     public SetupInfo setup;
     private HeightInfo heights;
+    private WidthInfo widths;
     private PositionInfo positions;
     private AnimationInfo anim;
     private DragInfo drag;
 
     private VerticalDragHandler vertical_drag_handler;
+    private RightLayoutDragHandler horizontal_right_drag_handler;
 
     private boolean first_time = true;
     private boolean reset = false;
+    private int rightWidthMeasureSpec;
 
     public ExpandableItem(Context context) {
 
@@ -84,25 +87,34 @@ public class ExpandableItem extends View {
         context = cxt;
         setup = stp;
         heights = new HeightInfo();
+        widths = new WidthInfo();
         positions = new PositionInfo();
         drag = new DragInfo();
         anim = new AnimationInfo();
 
         vertical_drag_handler = new VerticalDragHandler();
+        horizontal_right_drag_handler = new RightLayoutDragHandler();
     }
 
     @Override
     protected void onMeasure(int width_measure_spec, int height_measure_spec) {
 
-        super.onMeasure(width_measure_spec, width_measure_spec);
+        super.onMeasure(width_measure_spec, height_measure_spec);
 
         int visible_measure_spec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
         setup.center_layout.measure(width_measure_spec, visible_measure_spec);
+        widths.center_width = setup.center_layout.getMeasuredWidth();
         heights.center_height = setup.center_layout.getMeasuredHeight();
+
+        widths.canvas_width = widths.center_width;
 
         int hidden_measure_spec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
         setup.bottom_layout.measure(width_measure_spec, hidden_measure_spec);
         heights.bottom_height = setup.bottom_layout.getMeasuredHeight();
+
+        int right_measure_spec = MeasureSpec.makeMeasureSpec(widths.canvas_width, MeasureSpec.AT_MOST);
+        setup.right_layout.measure(right_measure_spec, hidden_measure_spec);
+        widths.right_width = setup.right_layout.getMeasuredWidth();
 
         heights.canvas_min_height = heights.center_height;
         heights.canvas_max_height = heights.center_height + heights.bottom_height;
@@ -133,6 +145,23 @@ public class ExpandableItem extends View {
         setMeasuredDimension(MeasureSpec.getSize(width_measure_spec), heights.canvas_height);
     }
 
+    public int getRightWidthMeasureSpec() {
+
+        int right_width = setup.right_layout.getLayoutParams().width;
+        if (right_width == ViewGroup.LayoutParams.WRAP_CONTENT) {
+
+            return MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+
+        } else if (right_width == ViewGroup.LayoutParams.MATCH_PARENT || right_width == ViewGroup.LayoutParams.FILL_PARENT) {
+
+            return MeasureSpec.makeMeasureSpec(widths.canvas_width, MeasureSpec.AT_MOST);
+
+        } else {
+
+            return MeasureSpec.makeMeasureSpec(right_width, MeasureSpec.EXACTLY);
+        }
+    }
+
     private void resetValues() {
 
         int canvas_y = 0;
@@ -142,9 +171,14 @@ public class ExpandableItem extends View {
         positions.max_bottom_y = heights.center_height;
         positions.min_bottom_y = positions.bottom_y;
 
+        positions.right_x = widths.canvas_width;
+        positions.max_right_x = widths.canvas_width - widths.right_width;
+        positions.min_right_x = positions.right_x;
+
         if (setup.is_expanded) positions.bottom_y = positions.max_bottom_y;
 
-        drag.drag_fling_threshold = heights.bottom_height * 0.2f;
+        drag.drag_fling_threshold_bottom = heights.bottom_height * 0.2f;
+        drag.drag_fling_threshold_right = widths.right_width * 0.2f;
 
         if (first_time) first_time = false;
         if (reset) reset = false;
@@ -177,6 +211,7 @@ public class ExpandableItem extends View {
 
         setup.center_layout.layout(left, top, right, top + heights.center_height);
         setup.bottom_layout.layout(left, top, right, top + heights.bottom_height);
+        setup.right_layout.layout(left, top, left + widths.right_width, top + heights.center_height);
     }
 
     @Override
@@ -190,7 +225,7 @@ public class ExpandableItem extends View {
 
         } else {
 
-            redraw(canvas, positions.bottom_y);
+            redraw(canvas, positions.bottom_y, positions.right_x);
         }
 
         checkForOpenOrClose();
@@ -198,16 +233,20 @@ public class ExpandableItem extends View {
 
     private void animateCanvas(Canvas canvas) {
 
-        redraw(canvas, anim.bottom_y);
+        redraw(canvas, anim.bottom_y, anim.right_x);
         requestLayout();
         invalidate();
     }
 
-    private void redraw(Canvas canvas, int bottom_y) {
+    private void redraw(Canvas canvas, int bottom_y, int right_x) {
+
+        //int right = widths.canvas_width - 50;
 
         canvas.translate(0, bottom_y);
         setup.bottom_layout.draw(canvas);
-        canvas.translate(0, (bottom_y * -1));
+        canvas.translate(right_x, (bottom_y * -1));
+        setup.right_layout.draw(canvas);
+        canvas.translate((right_x * -1) - (widths.canvas_width - right_x), 0);
         setup.center_layout.draw(canvas);
     }
 
@@ -248,12 +287,17 @@ public class ExpandableItem extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
-        if (setup.bottom_layout != null) return vertical_drag_handler.onTouch(event);
+        if (setup.right_layout != null) return horizontal_right_drag_handler.onTouch(event);
         else return false;
     }
-
     private boolean should_notify_on_open = true;
     private boolean opening = false;
+
+    public void open(boolean animate, boolean notify_listener) {
+
+        if (!setup.is_expanded) openBottom(animate, notify_listener);
+    }
+
     private void openBottom(boolean animate, boolean notify_listener) {
 
         should_notify_on_open = notify_listener;
@@ -272,13 +316,32 @@ public class ExpandableItem extends View {
         invalidate();
     }
 
-    public void open(boolean animate, boolean notify_listener) {
+    private void openRight(boolean animate, boolean notify_listener) {
 
-        if (!setup.is_expanded) openBottom(animate, notify_listener);
+        should_notify_on_open = notify_listener;
+        opening = true;
+
+        if (animate) {
+            anim.animating = true;
+            anim.anim_multiplier = widths.canvas_width / 15;
+            anim.right_x = positions.right_x;
+            positions.right_x = positions.max_right_x;
+        } else {
+            positions.right_x = positions.max_right_x;
+            anim.right_x = positions.right_x;
+        }
+
+        requestLayout();
+        invalidate();
     }
-
     private boolean should_notify_on_close = true;
     private boolean closing = false;
+
+    public void close(boolean animate, boolean notify_listener) {
+
+        if (setup.is_expanded) closeBottom(animate, notify_listener);
+    }
+
     private void closeBottom(boolean animate, boolean notify_listener) {
 
         should_notify_on_close = notify_listener;
@@ -297,9 +360,22 @@ public class ExpandableItem extends View {
         invalidate();
     }
 
-    public void close(boolean animate, boolean notify_listener) {
+    private void closeRight(boolean animate, boolean notify_listener) {
 
-        if (setup.is_expanded) closeBottom(animate, notify_listener);
+        should_notify_on_close = notify_listener;
+        closing = true;
+        if (animate) {
+            anim.animating = true;
+            anim.anim_multiplier = widths.canvas_width / 15;
+            anim.right_x = positions.right_x;
+            positions.right_x = positions.min_right_x;
+        } else {
+            positions.right_x = positions.min_right_x;
+            anim.right_x = positions.right_x;
+        }
+
+        requestLayout();
+        invalidate();
     }
 
     public void reset() {
@@ -320,7 +396,7 @@ public class ExpandableItem extends View {
 
         public View center_layout; // is the visible layout
         public View bottom_layout; // is the hidden layout that can be dragged down
-        public View right_layout; //is the hidden layout that can be dragged from the right
+        public View right_layout; // is the hidden layout that can be dragged from the right
 
         public int id = -1;
         public long long_press_delay = 2 * 1000;
@@ -351,23 +427,39 @@ public class ExpandableItem extends View {
         public int ht_diff_cntr_nd_bttm = 0;
     }
 
+    private class WidthInfo {
+
+        public int canvas_width = 0;
+
+        public int center_width = 0;
+        public int right_width = 0;
+
+        public int wd_diff_cntr_nd_rght = 0;
+    }
+
     private class PositionInfo {
 
         public int bottom_y = 0;
         public int max_bottom_y = 0;
         public int min_bottom_y = 0;
+
+        public int right_x = 0;
+        public int max_right_x = 0;
+        public int min_right_x = 0;
     }
 
     private class AnimationInfo {
 
         public boolean animating = false;
         public int bottom_y = 0;
+        public int right_x = 0;
         public int anim_multiplier = 4;
     }
 
     private class DragInfo {
 
-        public float drag_fling_threshold = 0;
+        public float drag_fling_threshold_bottom = 0;
+        public float drag_fling_threshold_right = 0;
     }
 
     private enum DIRECTION {UP, DOWN, LEFT, RIGHT};
@@ -459,13 +551,13 @@ public class ExpandableItem extends View {
 
             if (is_crct_nmbr_of_pointers) {
 
-                if (Math.abs(change_y) > drag.drag_fling_threshold) {
-                    if (dir == DIRECTION.DOWN) openBottom(true, true);
-                    if (dir == DIRECTION.UP) closeBottom(true, true);
+                if (Math.abs(change_y) > drag.drag_fling_threshold_bottom) {
+                    if (dir == DIRECTION.DOWN) openBottom(true, (!setup.is_expanded));
+                    if (dir == DIRECTION.UP) closeBottom(true, (setup.is_expanded));
                 }
-                if (Math.abs(change_y) < drag.drag_fling_threshold) {
-                    if (dir == DIRECTION.DOWN) closeBottom(true, false);
-                    if (dir == DIRECTION.UP) openBottom(true, false);
+                if (Math.abs(change_y) < drag.drag_fling_threshold_bottom) {
+                    if (dir == DIRECTION.DOWN) closeBottom(true, (setup.is_expanded));
+                    if (dir == DIRECTION.UP) openBottom(true, (!setup.is_expanded));
                 }
 
             } else if (setup.long_press_expand) {
@@ -481,6 +573,113 @@ public class ExpandableItem extends View {
                     }
                 }
             }
+        }
+    }
+
+    private class RightLayoutDragHandler {
+
+        private long down_time = 0;
+        private float x1 = 0;
+        private float x2 = 0;
+        private float dn_x = 0;
+        private float up_x = 0;
+        private boolean is_crct_nmbr_of_pointers = false;
+        private DIRECTION dir;
+
+        public boolean onTouch(MotionEvent event) {
+
+            int action = event.getAction();
+
+            switch (action) {
+
+                case MotionEvent.ACTION_DOWN:
+
+                    handleMotionDown(event);
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+
+                    //int finger_touch_count = event.getPointerCount();
+                    //if (finger_touch_count == setup.drag_gesture.value) {
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                        handleMotionMove(event);
+                        //is_crct_nmbr_of_pointers = true;
+                    //}
+                    break;
+
+                case MotionEvent.ACTION_UP:
+
+                    handleMotionUp(event);
+                    //is_crct_nmbr_of_pointers = false;
+                    break;
+            }
+
+            return true;
+        }
+
+        private void handleMotionDown(MotionEvent event) {
+
+            down_time = System.currentTimeMillis();
+            x1 = event.getRawX();
+            dn_x = x1;
+            if (positions.right_x == positions.min_right_x) dir = DIRECTION.LEFT;
+            if (positions.right_x == positions.max_right_x) dir = DIRECTION.RIGHT;
+        }
+
+        private void handleMotionMove(MotionEvent event) {
+
+            if (anim.animating) {
+                anim.animating = false;
+                positions.right_x = anim.right_x;
+                opening = false; closing = false;
+            }
+
+            x2 = event.getRawX();
+            float change_x = x2 - x1;
+
+            if (change_x < 0) dir = DIRECTION.LEFT;
+            if (change_x > 0) dir = DIRECTION.RIGHT;
+
+            positions.right_x = positions.right_x + (int) change_x;
+            if (positions.right_x < positions.max_right_x) positions.right_x = positions.max_right_x;
+            if (positions.right_x > positions.min_right_x) positions.right_x = positions.min_right_x;
+
+            x1 = x2;
+
+            requestLayout();
+            invalidate();
+        }
+
+        private void handleMotionUp(MotionEvent event) {
+
+            up_x = event.getRawX();
+            float change_y = up_x - dn_x;
+
+            //if (is_crct_nmbr_of_pointers) {
+
+                if (Math.abs(change_y) > drag.drag_fling_threshold_right) {
+                    if (dir == DIRECTION.LEFT) openRight(true, (!setup.is_expanded));
+                    if (dir == DIRECTION.RIGHT) closeRight(true, (setup.is_expanded));
+                }
+                if (Math.abs(change_y) < drag.drag_fling_threshold_right) {
+                    if (dir == DIRECTION.LEFT) closeRight(true, (setup.is_expanded));
+                    if (dir == DIRECTION.RIGHT) openRight(true, (!setup.is_expanded));
+                }
+
+            //}
+//            else if (setup.long_press_expand) {
+//
+//                long up_time = System.currentTimeMillis();
+//                long delay = up_time - down_time;
+//                if (delay >= setup.long_press_delay) {
+//
+//                    if (!setup.is_expanded && !anim.animating) {
+//                        open(true, true);
+//                    } else if (setup.is_expanded && !anim.animating) {
+//                        close(true, true);
+//                    }
+//                }
+//            }
         }
     }
 }
